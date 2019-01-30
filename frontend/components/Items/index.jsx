f9a7f2995e46a63d304cf0dfafdc42aaa83fc7ef
Adam Mescher <admescher@gmail.com>
2019-01-29 23:26:43 -0700

    V16 in-progress -- create components/Items/index.jsx

---
+++ frontend/components/Items/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+
+class Items extends Component {
+    render() {
+        return (
+            <div>Items!</div>
+        )
+    }
+}
+
+export default Items;
