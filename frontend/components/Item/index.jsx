4389a9da639ca89d6c3e79f5980e4bce4475de69
Adam Mescher <admescher@gmail.com>
2019-02-17 16:16:28 -0700

    remove ItemStyles.js and import all new components into Item component

--- frontend/components/Item/index.jsx
+++ frontend/components/Item/index.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import DeleteItem from '../DeleteItem/index';
-import Title from '../styles/Title';
 import StyledItem from './styled';
-import PriceTag from '../styles/PriceTag';
-import formatMoney from '../../lib/formatMoney';
+import DeleteItem from '../DeleteItem/index';
+import Title from '../Title/index';
+import PriceTag from '../PriceTag/styled';
 export default class Item extends Component {
   static propTypes = {
     item: PropTypes.object.isRequired
@@ -16,17 +15,8 @@ export default class Item extends Component {
     return (
       <StyledItem>
         {item.image && <img src={item.image} alt={item.title} />}
-        <Title>
-          <Link
-            href={{
-              pathname: '/item',
-              query: { id: item.id }
-            }}
-          >
-            <a>{item.title}</a>
-          </Link>
-        </Title>
-        <PriceTag>{formatMoney(item.price)}</PriceTag>
+        <Title item={item} />
+        <PriceTag price={item.price} />
         <p>{item.description}</p>
         <div className="buttonList">
           <Link
